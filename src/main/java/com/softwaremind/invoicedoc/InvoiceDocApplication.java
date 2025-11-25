fe7a599f84beaf279436d26666f6d5ca9bd9e53d
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-11-25 19:25:52 +0100

    Initial commit

---
+++ src/main/java/com/softwaremind/invoicedoc/InvoiceDocApplication.java
@@ -0,0 +1,13 @@
+package com.softwaremind.invoicedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceDocApplication.class, args);
+    }
+
+}
