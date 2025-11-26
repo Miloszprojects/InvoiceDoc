c5a8abed41be1af72eb50c49b653b8ac3793ca5e
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-11-26 10:20:52 +0100

    Configuration of project with setup of modules

--- invoicedoc-backend/src/main/java/com/softwaremind/invoicedocbackend/InvoiceDocBackendApplication.java
+++ invoicedoc-backend/src/main/java/com/softwaremind/invoicedocbackend/InvoiceDocBackendApplication.java
@@ -1,13 +1,13 @@
-package com.softwaremind.invoicedoc;
+package com.softwaremind.invoicedocbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InvoiceDocApplication {
+public class InvoiceDocBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InvoiceDocApplication.class, args);
+        SpringApplication.run(InvoiceDocBackendApplication.class, args);
     }
 
 }
---
+++ invoicedoc-backend/src/test/java/com/softwaremind/invoicedocbackend/InvoiceDocBackendApplicationTests.java
@@ -0,0 +1,36 @@
+package com.softwaremind.invoicedocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+class InvoiceDocBackendApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16-alpine")
+                    .withDatabaseName("invoicedoc")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        System.out.println(">>> Testcontainers JDBC URL = " + postgres.getJdbcUrl());
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
